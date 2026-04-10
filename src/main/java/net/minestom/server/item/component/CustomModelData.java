89dcbdcc150cd75a163cbe83dcadb6bf77f71b22
Ambrosia <tatteaid@gmail.com>
2026-04-10 13:58:16 -0500

    Fix crash exploit

--- src/main/java/net/minestom/server/item/component/CustomModelData.java
+++ src/main/java/net/minestom/server/item/component/CustomModelData.java
@@ -13,18 +13,24 @@ public record CustomModelData(
         List<Float> floats, List<Boolean> flags,
         List<String> strings, List<RGBLike> colors
 ) {
+
+    private static final int MAX_ENTRIES = 256;
+
     public static final NetworkBuffer.Type<CustomModelData> NETWORK_TYPE = NetworkBufferTemplate.template(
-            NetworkBuffer.FLOAT.list(), CustomModelData::floats,
-            NetworkBuffer.BOOLEAN.list(), CustomModelData::flags,
-            NetworkBuffer.STRING.list(), CustomModelData::strings,
-            Color.NETWORK_TYPE.list(), CustomModelData::colors,
-            CustomModelData::new);
+            NetworkBuffer.FLOAT.list(MAX_ENTRIES), CustomModelData::floats,
+            NetworkBuffer.BOOLEAN.list(MAX_ENTRIES), CustomModelData::flags,
+            NetworkBuffer.STRING.list(MAX_ENTRIES), CustomModelData::strings,
+            Color.NETWORK_TYPE.list(MAX_ENTRIES), CustomModelData::colors,
+            CustomModelData::new
+    );
+
     public static final Codec<CustomModelData> CODEC = StructCodec.struct(
             "floats", Codec.FLOAT.list().optional(List.of()), CustomModelData::floats,
             "flags", Codec.BOOLEAN.list().optional(List.of()), CustomModelData::flags,
             "strings", Codec.STRING.list().optional(List.of()), CustomModelData::strings,
             "colors", Color.CODEC.list().optional(List.of()), CustomModelData::colors,
-            CustomModelData::new);
+            CustomModelData::new
+    );
 
     public CustomModelData {
         floats = List.copyOf(floats);
